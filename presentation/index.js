501589c6d46f67ca7a75abfd6273df3f2c3edea7
Rachel Engle <u1007081@Rachel-E.local>
2017-04-04 20:42:37 -0600

    styles and photos

--- presentation/index.js
+++ presentation/index.js
@@ -34,7 +34,11 @@ const images = {
   logo: require("../assets/formidable-logo.svg"),
   markdown: require("../assets/markdown.png"),
   pinkybrain: require("../assets/pink-n-brain-todolist.jpg"),
-  stretch: require('../assets/stretching.jpg')
+  stretch: require('../assets/stretching.jpg'),
+  signup: require('../assets/signup.png'),
+  calgraph: require('../assets/calorie-graph.png'),
+  mealform: require('../assets/meals-form.png'),
+  finnerlogo: require('../assets/logo-finner@2x.png')
 };
 
 preloader(images);
@@ -42,9 +46,8 @@ preloader(images);
 const theme = createTheme({
   primary: "white",
   secondary: "#000000",
-  tertiary: "#ef6c00",
-  quartenary: "#26a69a",
-  lightgray: "#999"
+  tertiary: "#f15b2a",
+  quartenary: "#26a69a"
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -55,26 +58,22 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["fade"]} bgColor="secondary">
-          <Heading>Finner</Heading>
+          <Image src={images.finnerlogo} style={{ width:'200px'}}></Image>
+          <Heading>FINNER</Heading>
           <Text margin="10px 0 0" textColor="primary" size={1} bold>
-            a better fitness planner
+            a better meal planner
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="lightgray">
-          <Heading size={6} textColor="primary" caps>The Team</Heading>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="tertiary" caps>The Team</Heading>
           <Text>
             Eugene, Jared, and Rachel
           </Text>
-          <Text>&nbsp;</Text>
-          <Heading size={6} textColor="primary" caps>The Vision</Heading>
-          <Text>
-            Take over the world
-          </Text>
         </Slide>
         <Slide transition={["fade"]} bgImage={images.stretch} bgDarken={0.55} textColor="primary" align="flex-start">
-          <Heading size={6} textColor="tertiary" caps>The Problem</Heading>
-          <Appear><Text size={9} textColor="tertiary">Diets Suck</Text></Appear>
-          <List>
+          <Heading size={6} textColor="primary" caps>The Problem</Heading>
+          <Appear><Text size={5} textColor="primary" style={{textDecoration:'1px solid #f15b2a' }} >Diets suck.</Text></Appear>
+          <List style={{padding: '0 6em'}} listStylePosition="inside">
             <Appear><ListItem>The food sucks</ListItem></Appear>
             <Appear><ListItem>The amount sucks</ListItem></Appear>
             <Appear><ListItem>The result sucks</ListItem></Appear>
@@ -83,7 +82,7 @@ export default class Presentation extends React.Component {
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>The Problem Cont...</Heading>
           <Appear><Text size={9} text color="tertiary">Why does everything suck?</Text></Appear>
-          <List listStylePosition="inside">
+          <List listStylePosition="inside" style={{padding: '0 4em'}}>
             <Appear><ListItem>Eating too little</ListItem></Appear>
             <Appear><ListItem>Not tracking progress</ListItem></Appear>
             <Appear><ListItem>Choosing the wrong food</ListItem></Appear>
@@ -92,40 +91,40 @@ export default class Presentation extends React.Component {
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Find Your Daily Calories</Heading>
           <List>
-            {/*Add in a video of what it looks like to signup*/}
+            <Image style={{ width: '80%', margin:'0 auto', display: 'block'}} src={images.signup}></Image>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Track your progress</Heading>
           <List>
-            {/*Add video or screen shot of what the dashboard looks like when updating*/}
-            <Appear><ListItem>Track your Progress</ListItem></Appear>
+            <Image style={{ width: '80%', margin:'0 auto', display: 'block'}} src={images.calgraph}></Image>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Get the Right Food</Heading>
           <List>
-            {/*Meal plan and how it works*/}
-            <Appear><ListItem>Track your Progress</ListItem></Appear>
+            <Image style={{ width: '80%', margin:'0 auto', display: 'block'}} src={images.mealform}></Image>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>The future</Heading>
-          <List>
+          <List style={{padding: '0 6em'}}>
             <Appear><ListItem>Dietary Feedback</ListItem></Appear>
             <Appear><ListItem>Increased Accuracy</ListItem></Appear>
             <Appear><ListItem>Workout Tracking</ListItem></Appear>
             <Appear><ListItem>Individual Expertise</ListItem></Appear>
           </List>
-          <Appear>
-            <Image src={images.pinkybrain}></Image>
-          </Appear>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Any Questions?</Quote>
-            <Cite>Finner</Cite>
-          </BlockQuote>
+        <Slide transition={["fade"]} bgColor="secondary">
+          
+          <Image src={images.finnerlogo} style={{ width:'200px'}}></Image>
+          <Heading>FINNER</Heading>
+          <Text margin="10px 0 0" textColor="primary" size={1} bold>
+            a better meal planner
+          </Text>
+          <Text textColor="primary" size={5}>
+            Eugene, Jared, and Rachel
+          </Text>
         </Slide>
       </Deck>
     );
